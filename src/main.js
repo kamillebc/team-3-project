667d63d9152edbc876ce12d3a13968e48fbacc39
NikhilK017 <kyadiggerin9868@my.sduhsd.net>
2025-04-16 10:12:20 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+function logout() {
+    window.location.href = 'login.html';
+  }
+  
+  function goTo(page) {
+    window.location.href = page;
+  }
+  
+  function addFriends() {
+    alert("Friend popup coming soon.");
+  }
+  
+  function addEvent() {
+    alert("Add event popup coming soon.");
+  }
